8243aa8fc926a2b94d12d794b0809b6b3bf0ed10
Emmanuel Casimir <ecasimir@bostonk12.org>
2018-03-23 18:52:50 -0400

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-# codeWarsSolution7
 // Write a method sum (sum_array in python, SumArray in C#) that takes an array of numbers and returns the sum of the numbers. These may be integers or decimals for Ruby and any instance of Num for Haskell. The numbers can also be negative. If the array does not contain any numbers then you should return 0.
 //
 // Examples
@@ -9,3 +8,10 @@
 // You can assume that you are only given numbers.
 // You cannot assume the size of the array.
 // You can assume that you do get an array and if the array is empty, return 0.
+// What We're Testing
+// We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
+
+function sum (numbers) {
+    var sum = numbers.reduce(function(a, b) { return a + b; }, 0);
+    return sum
+};
